a049256739ac883f226449e72d88216862093deb
Israfil <mdronnieronniers@gmial.com>
2023-11-24 20:10:00 +0600

    interface added

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+import { Model } from 'mongoose';
+
+export type TOrder = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export interface TUser {
+  userId: number;
+  username: string;
+  password: string;
+  email: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  orders: TOrder[];
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+}
+
+// * for creating static method
+export interface UserModel extends Model<TUser> {
+  isUserExists(id: string): Promise<TUser | null>;
+}
